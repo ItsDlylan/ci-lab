8d7a52ca8bc206f55d6f01f60c6867c122ea5709
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-30 14:48:12 -0700

    Testing to see if the pull request gets checked before merge.

--- test.js
+++ test.js
@@ -7,3 +7,7 @@ test('can create a basic greeting', () => {
 test('Can subtract 2 numbers', () => {
 	expect(tests.subtractNum(5, 2)).toBe(3)
 })
+
+test('Can subtract 2 numbers 2 and 2', () => {
+	expect(tests.subtractNum(2, 2)).toBe(0)
+})
